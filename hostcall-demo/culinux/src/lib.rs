22cee69693e7bdf68252073896d10a57b4529595
Christian Legnitto <christian@legnitto.com>
2025-11-20 23:34:36 -0800

    Fix culinux PTX rebuild wiring

--- hostcall-demo/culinux/src/lib.rs
+++ hostcall-demo/culinux/src/lib.rs
@@ -10,7 +10,15 @@ use std::io::Write;
 #[unsafe(no_mangle)]
 pub extern "gpu-kernel" fn kernel_main() {
     let msg = format!("This file was created *from a GPU*, using the Rust standard library :D\n We are {:?}", "VectorWare".to_owned());
-    std::fs::File::create("rust_from_gpu.txt").unwrap().write_all(msg.as_bytes()).unwrap();
+    std::fs::File::create("rust_from_gpu.txt")
+        .unwrap()
+        .write_all(msg.as_bytes())
+        .unwrap();
+    // Second write to confirm kernel continues normally without exit.
+    std::fs::File::create("rust_from_gpu6.txt")
+        .unwrap()
+        .write_all(msg.as_bytes())
+        .unwrap();
 }
 #[used]
 static KEEP_KERNEL_MAIN: &[extern "gpu-kernel" fn() -> ()] = &[kernel_main];
